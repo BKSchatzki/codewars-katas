41663348c6fb27455a739d8753d3f9b900a2b028
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-05-01 14:59:49 -0700

    Create 8k-multiply.js

---
+++ 8k-multiply.js
@@ -0,0 +1,10 @@
+// Simple function that takes two number arguments and returns their product
+
+function multiply(a, b){
+  let product = a * b;
+  return product;
+}
+
+// I could have:
+  // Returned directly without creating variable.
+  // Added a check for non-number arguments.
