9e7039b3f761e28eb03267ee828668ee0383549d
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-05-02 16:13:03 -0700

    Create 8k-myHeadIsAtTheWrongEnd.js

---
+++ 8k-myHeadIsAtTheWrongEnd.js
@@ -0,0 +1,7 @@
+// Use the reverse() method to reverse the array.
+
+function fixTheMeerkat(arr) {
+  return arr.reverse();
+}
+
+// I swear I've also done a challenge where I did this without the reverse method.
