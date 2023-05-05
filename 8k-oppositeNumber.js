1719e18f87642bb25d531af06ad097dbfbcb9364
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-05-04 21:55:09 -0700

    Create 8k-oppositeNumber.js

---
+++ 8k-oppositeNumber.js
@@ -0,0 +1,9 @@
+// This function returns the opposite of the given number. For example, if given 10 returns -10.
+
+function opposite(number) {
+  // Assign new value to number, by multiplying by two and subtracting it.
+  number = number - (number * 2);
+  return number;
+}
+
+// Clearly I could have just returned -number, but oh well.
