4e93c718ba7e4cebcb266b792028396dc04ccb10
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-05-07 02:31:06 -0700

    Return the opposite of the given number

--- 8k-oppositeNumber.js
+++ 8k-oppositeNumber.js
@@ -1,9 +1,22 @@
-// This function returns the opposite of the given number. For example, if given 10 returns -10.
+/* Parameters, returns, examples
+- Take a number
+- Return its opposite
+- 10 returns -10
+  105 returns -105
+*/
 
+/* Pseudocode walkthrough
+- Reassign number to self minus doubled self
+*/
+
+// Original solution submitted to Codewars
 function opposite(number) {
   // Assign new value to number, by multiplying by two and subtracting it.
   number = number - (number * 2);
   return number;
 }
 
-// Clearly I could have just returned -number, but oh well.
+/* Reflection
+- I should have just returned -number, but I wanted to try a mathematical solution
+- Can also add validation to alert user if argument isn't a number or if NaN is returned
+*/
