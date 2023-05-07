022bdafabbfeadb4cd2596ac0336924d3b6ee1d3
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-05-07 02:38:03 -0700

    Return given array with indexes reversed

--- 8k-myHeadIsAtTheWrongEnd.js
+++ 8k-myHeadIsAtTheWrongEnd.js
@@ -1,7 +1,21 @@
-// Use the reverse() method to reverse the array.
+/* Parameters, returns, examples
+- Take any array
+- Return it in reversed order
+- ["Hi", 4, "there"] returns ["there", 4, "Hi"]
+*/
 
+/* Pseudocode walkthrough
+- Return the given array with the reverse method on it
+*/
+
+// Original solution submitted to Codewars
 function fixTheMeerkat(arr) {
   return arr.reverse();
 }
 
-// I swear I've also done a challenge where I did this without the reverse method.
+/* Reflection
+- This is pretty straightforward, but there are ways to do this manually
+  Could use decrementing for-loop and push to new array
+  Could use incrementing for-loop and unshift to new array
+  Could map to new array and access array inside callback function
+*/
