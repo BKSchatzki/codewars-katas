ac5b80f696dc947b6c68a836c340e850371f2cdf
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-05-09 19:10:37 -0700

    Return product of two numbers

--- 8k-multiply.js
+++ 8k-multiply.js
@@ -1,10 +1,22 @@
-// Simple function that takes two number arguments and returns their product
+/* Parameters, returns, examples
+- Take two numbers
+- Return their product
+- (1, 1) returns 1
+  (0, 1000000) returns 0
+  (2, 4) returns 8
+*/
+
+/* Pseudocode walkthrough
+- Create variable product and assign to two parameters multiplied
+- Return product
+*/
 
 function multiply(a, b){
   let product = a * b;
   return product;
 }
 
-// I could have:
-  // Returned directly without creating variable.
-  // Added a check for non-number arguments.
+/* Reflection
+- Could have returned directly without creating local variable
+- Can add check for non-number arguments and alert user if either isn't
+*/
