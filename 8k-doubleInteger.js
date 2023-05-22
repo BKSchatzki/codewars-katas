a47e9627f52f6c008abe1b5bf847de42d922cef1
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-05-21 23:41:05 -0700

    Return an integer's value, doubled

---
+++ 8k-doubleInteger.js
@@ -0,0 +1,20 @@
+/* Parameters, returns, examples
+- Enter an integer
+- Return that integer doubled
+- (2) returns 4
+- (4) returns 8
+- (365) returns 730
+*/
+
+/* Pseudocode walkthrough
+- Return two times the integer
+*/
+
+function doubleInteger(i) {
+  return 2 * i;
+}
+
+/* Reflection
+- Honestly, what is there to do.
+- I could be more explicit and reassign i to a doubled value before returning if I wanted to be more explicit.
+*/
